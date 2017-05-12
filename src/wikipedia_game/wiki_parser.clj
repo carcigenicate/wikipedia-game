fe585026ce6344f25c84eb932f7820e350e215da
carcigenicate <slomifier@gmail.com>
2017-05-12 06:02:47 -0600

    Changed to manual parsing from using enlive's selectors for the specific bits. Turned out to be much easier once I had narrowed down the search.

--- src/wikipedia_game/wiki_parser.clj
+++ src/wikipedia_game/wiki_parser.clj
@@ -1,7 +1,9 @@
 (ns wikipedia-game.wiki-parser
   (:require [net.cgrand.enlive-html :as h]
-            [clojure.string :as s])
-  (:import [java.net URL]))
+            [clojure.string :as s]
+            [clojure.java.io :as jio])
+  (:import [java.net URL]
+           (java.io FileNotFoundException)))
 
 (def base-page-link "https://en.wikipedia.org/wiki/")
 
@@ -24,12 +26,23 @@
   (construct-page-link
     (linkify-text text)))
 
+(defn main-body-links [parsed-html]
+  (h/select parsed-html [:#mw-content-text :p :> :a]))
+
+(defn remove-internal-links [links]
+  (->> links
+    (remove #(-> % :attrs :href (first) (= \#)))))
+
+(defn filtered-links [parsed-html]
+  (-> (main-body-links parsed-html)
+    (remove-internal-links)))
+
 (defn get-first-link-partial
   "Takes a page link and returns the first partial link in the main body.
   Returns nil if the body doesn't contain any links."
   [link]
   (let [page (parse-page link)
-        first-link? (first (h/select page [:#mw-content-text :p :> :a]))]
+        first-link? (first (filtered-links page))]
     (if first-link?
       (first (h/attr-values first-link? :href))
       nil)))
@@ -41,26 +54,36 @@
     (extract-link-text)
     (construct-page-link)))
 
+(defn try-get-next-page-name [link]
+  (try
+    (let [next-link-partial (get-first-link-partial link)]
+      (extract-link-text next-link-partial))
+
+    (catch FileNotFoundException e
+      nil)))
+
 (defn link-jump [seed-term max-jumps]
   (loop [i 0
          current-link (simple-construct-page-link seed-term)
          visited #{(linkify-text seed-term)}
          acc []]
+    (let [next-page-name? (try-get-next-page-name current-link)
+          acc' (conj acc next-page-name?)]
 
-    (let [next-link-partial (get-first-link-partial current-link)
-          next-page-name (extract-link-text next-link-partial)
-          next-link (construct-page-link next-page-name)
-          acc' (conj acc next-page-name)]
+      (cond
+        (not next-page-name?)
+        (conj acc :bad-page)
 
-      (if (or (> i max-jumps) (visited next-page-name))
+        (or (> i max-jumps) (visited next-page-name?))
         acc'
 
+        :else
         (recur (inc i)
-               next-link
-               (conj visited next-page-name)
+               (construct-page-link next-page-name?)
+               (conj visited next-page-name?)
                acc')))))
 
-#_
 (def test-page
-  (parse-page (simple-construct-page-link "Tiger Woods")))
+  (delay
+    (parse-page (simple-construct-page-link "Soft Drink"))))
 
